07c7609f2af37432f0853300cf56a5e89d096f4e
Oliver Brandon Bravo Cedillo <oliver.bravoc@gmail.com>
2021-05-13 16:24:44 -0500

    feat: project initialization

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <p data-testid="home-title">It works</p>
+    </div>
+  );
+}
+
+export default App;
